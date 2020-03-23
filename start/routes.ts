64983b2228519198760c119520121b741f2255ce
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2020-03-22 23:28:25 -0300

    :rocket: Cria posts

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,5 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.get('/posts', 'PostsController.index')
+Route.post('/posts', 'PostsController.store')
